34240ce92f023444d17246c05f7ed4509932ca5e
choiseowu <chlthdud28@naver.com>
2023-06-08 15:14:46 +0900

    ✨ sidebar open,close 콜백함수

--- 230601_맥도날드실습/js/sidebar.js
+++ 230601_맥도날드실습/js/sidebar.js
@@ -12,7 +12,7 @@ console.log(gnbIconBtn,sidebar,sidebarOverlay);
 //   // 실행 할 코드 
 // }
 
-function openSidebar() {
+// function openSidebar() {
   // 우리가 하고 싶은 내용
   // 1. gnbIconBtn 클릭 했을 때 sidebar, sidebarOverlay한테 is-active 클래스 추가 
   
@@ -20,18 +20,35 @@ function openSidebar() {
   // DOM이란 자바스크립트 같은 스크립팅 언어가 우리가 만들어 놓은 웹 페이지에 접근해서 그 요소를 쉽게 조작하는 역할을 한다.
   // 현재 웹 브라우저에서 DOM을 조작하는 언어는 Javascript가 유일하다.
   // DOM 데이터 타입(메소드, 프로퍼티, 이벤트 리스너, 스타일 등)
-  sidebar.classList.add('is-active');
-  sidebarOverlay.classList.add('is-active');
-}
+//   sidebar.classList.add('is-active');
+//   sidebarOverlay.classList.add('is-active');
+// }
 
 // 내가 gnbIconBtn한테 이벤트를 하나 추가할건데, => click 이벤트를 추가하고 openSidebar 이 함수를 실행시켜라
-gnbIconBtn.addEventListener('click', openSidebar);
+// gnbIconBtn.addEventListener('click', openSidebar);
 
 
-function closeSidebar() {
+// function closeSidebar() {
   // is-active 클래스 제거
+//   sidebar.classList.remove('is-active');
+//   sidebarOverlay.classList.remove('is-active');
+// }
+
+// sidebarOverlay.addEventListener('click', closeSidebar);
+
+// 콜백함수 : 함수를 활용하는 하나의 방식
+// 변수의 유효범위(scope), 동기 / 비동기처리 
+// 코드가 작성된 순서대로 동작(가독성, 재사용성이 좋다)
+// 함수를 호출하는 시점이나, 코드가 동작하는 순서가 바뀐다.
+
+// gnbIconBtn 클릭 했을 때 실행 할 함수
+gnbIconBtn.addEventListener('click', function(){
+  // 실행 할 코드
+  sidebar.classList.add('is-active');
+  sidebarOverlay.classList.add('is-active');
+})
+
+sidebarOverlay.addEventListener('click', function(){
   sidebar.classList.remove('is-active');
   sidebarOverlay.classList.remove('is-active');
-}
-
-sidebarOverlay.addEventListener('click', closeSidebar);
+})
