5260c32206452bde1e454cbe05ba832a5c1a54b0
choiseowu <chlthdud28@naver.com>
2023-07-14 15:32:21 +0900

    ✨ button에 active class add

--- 230711_HSFF실습/js/slide.js
+++ 230711_HSFF실습/js/slide.js
@@ -12,5 +12,14 @@ buttons.forEach(function(button, index){
     // translateX(-100vw)
     // slideList.style.transform = 'translateX(' + translateValue +')'
     slideList.style.transform = `translateX(${translateValue})`
+
+
+    // buttons라는 배열 안에 모든 요소한테 active class remo
+    buttons.forEach(function(otherBtn){
+      otherBtn.classList.remove('active');
+    })
+
+    // 클릭된 요소한테만 active class add 
+    button.classList.add('active');
   })
 })
