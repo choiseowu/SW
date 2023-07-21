b85eeadb96868214b03215ca4e52eb209578229e
choiseowu <chlthdud28@naver.com>
2023-07-21 14:44:26 +0900

    ✨ slide4 setInterval 기능 구현 완료

--- 230706_slide/slide4(setInterval)/js/script.js
+++ 230706_slide/slide4(setInterval)/js/script.js
@@ -44,3 +44,20 @@ function sliding(dir){
 }
 
 sliding(0)
+
+// 이전 슬라이드로 가야하니까 음수값을 매개변수로 전달
+$('#prev').click(function(){sliding(-1)});
+// 다음 슬라이드로 가야하니까 양수값을 매개변수로 전달 
+$('#next').click(function(){sliding(1)});
+
+
+// 자동으로 슬라이드 넘어가게
+let auto = setInterval(sliding, 5000, 1);
+
+
+// dots div를 클릭했을 때 해당 슬라이드로 이동
+$('#dots div').click(function(){
+  cur = 0;
+  let num = $(this).index()
+  sliding(num);
+})
