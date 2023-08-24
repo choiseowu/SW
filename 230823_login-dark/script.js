b33bcf8632d339bbffb97bacd4070ad085873587
choiseowu <chlthdud28@naver.com>
2023-08-24 16:14:08 +0900

    ✨ 삼항연산자로 Dark / Light mode toggle

--- 230823_login-dark/script.js
+++ 230823_login-dark/script.js
@@ -6,6 +6,7 @@
 // x, y => x + y; 
 
 let btnLog = document.querySelector('.btn-login');
+let darkToggle = document.getElementById('darkmode-toggle');
 
 document.querySelectorAll('.animate-input').forEach((e) => {
   // e : .animate-input class가진 값을 하나씩 떼서 넘겨준 값
@@ -49,3 +50,18 @@ document.querySelectorAll('.animate-input').forEach((e) => {
     }
   } 
 })
+
+// darkmode toggle 
+// darkToggle 클릭했을 때 
+// 텍스트 변경 : Darkmode -> Lightmode
+// body에 .dark class add 
+
+darkToggle.onclick = (e) => {
+  e.preventDefault();
+  let body = document.querySelector('body');
+  body.classList.toggle('dark');
+  
+  // 삼항 연산자 
+  // 조건 ? 참일 때 실행 할 코드 (true) : 거짓일 때 실행 할 코드 (false)
+  darkToggle.innerHTML = body.classList.contains('dark') ? 'Lightmode' : 'Darkmode';
+}
