c1da70c4e3af2a6ca80b92ee453d82c8eb55f906
choiseowu <chlthdud28@naver.com>
2023-09-06 15:46:01 +0900

    ✨ deserializeUser 해당 세션 데이터 찾기

--- express/index.js
+++ express/index.js
@@ -361,3 +361,11 @@ passport.use(new LocalStrategy({
 passport.serializeUser(function(user, done){
   done(null, user.id)
 })
+
+
+// 해당 세션 데이터를 login collection에서 찾는다
+passport.deserializeUser(function(id, done){
+  db.collection('login').findOne({id : id}, function(error, result){
+    done(null, result)
+  })
+})
