5bdaf947a9afdc56ae7d095fc3d9e9831f036f59
choiseowu <chlthdud28@naver.com>
2023-09-07 16:25:26 +0900

    ✨ dataTime 가장 최근 데이터 가져오기

--- 230907_미세먼지API/script.js
+++ 230907_미세먼지API/script.js
@@ -5,12 +5,13 @@ var queryParams = '?' + encodeURIComponent('serviceKey') + '='+'6h06MN4YuByhKlHD
 queryParams += '&' + encodeURIComponent('returnType') + '=' + encodeURIComponent('json'); /*응답 데이터 타입 설정*/
 queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /*한 페이지 결과 수*/
 queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /*페이지 번호 수*/
-queryParams += '&' + encodeURIComponent('sidoName') + '=' + encodeURIComponent('경북'); /*조회할 데이터 시도 이름 설정*/
+queryParams += '&' + encodeURIComponent('sidoName') + '=' + encodeURIComponent('대전'); /*조회할 데이터 시도 이름 설정*/
 queryParams += '&' + encodeURIComponent('searchCondition') + '=' + encodeURIComponent('DAILY'); /*데이터 기간*/
 
 xhr.open('GET', url + queryParams);
 
-xhr.onreadystatechange = function () {
+function updateData(){
+  xhr.onreadystatechange = function () {
     if (this.readyState == 4) {  // 서버 응답 완료 상태 확인 
       if(this.status === 200) {  // HTTP 상태 코드가 200(성공)
         // 서버로부터 받은 JSON 형식의 문자열 데이터를 Javascript 객체로 변환
@@ -22,13 +23,21 @@ xhr.onreadystatechange = function () {
         if(responsData.response.body.items) {
           let items = responsData.response.body.items;
           let dataDisplay = document.getElementById('data');
+          // 가장 최근 데이터를 저장 할 변수
+          // 의도적으로 해당 변수에 아무런 데이터가 할당되지 않았음을 나타내기 위해 null
+          let latestData = null;
           for(let i = 0; i < items.length; i++) {
             let item = items[i];
 
-            if(item.cityName == '경주시') {
-              let dataItem = document.createElement('div');
-              dataItem.innerHTML = item.cityName + '미세먼지 : ' + item.pm10Value;
-              dataDisplay.appendChild(dataItem);
+            if(item.cityName == '유성구') {
+              // dataTime 가장 최근 데이터 가져오기
+              // 
+              if(!latestData || item.dataTime > latestData.dataTime) {
+                latestData = item.dataTime;
+                let dataItem = document.createElement('div');
+                dataItem.innerHTML = item.cityName + '미세먼지 : ' + item.pm10Value;
+                dataDisplay.appendChild(dataItem);
+              }
             }
           }
         } else {
@@ -38,6 +47,8 @@ xhr.onreadystatechange = function () {
         console.log('HTTP 요청 실패' + this.status)
       }
     }
-};
+  };
+  xhr.send('');
+}
 
-xhr.send('');
+updateData();
