61a7e38384f91072d03852abbda44626cbb2db4a
choiseowu <chlthdud28@naver.com>
2023-09-20 13:06:56 +0900

    🚧 changeBg 함수 기능 구현중

--- React/react-basic/src/component/State.jsx
+++ React/react-basic/src/component/State.jsx
@@ -5,18 +5,25 @@ import { useState } from 'react';
 // React에서 상태를 관리하기 위한 Hook 중에 하나
 // state를 사용해서 상태를 변경하면, React가 
 // 변경된 상태를 감지해서 자동으로 컴포넌트를 재랜더링 한다.
-
 // useState()
 // Javascript 변수 다루듯이 값을 직정 수정 할 수 없다.
 // useState() 함수를 이용해서 값을 변경해야 한다.
-
 // 1. import {useState} from 'react'
 // 2. let [변수이름, set변수이름] = useState(초기값)
-
 const State = () => {
   // let num = 0;
   let [count, setCount] = useState(0);
   let [title, setTitle] = useState('리액트 너무 어려워요');
+  let [name, setName] = useState(['이름1', '이름2', '이름3']);
+  let [bg, setBg] = useState('white');
+
+
+  let changeBg = () => {
+    // 삼항연산자로 현재 배경색이 white라면
+    // 다른색으로 바꿔주고, 아니라면 white로 바꿔라
+
+  }
+  
   // count : 해당 상태값을 의미하는 변수
   // setCount : 해당 상태값을 변경하는 함수
   // 이 함수를 호출할 때 변경된 값을 인수로 전달한다.
@@ -25,18 +32,26 @@ const State = () => {
     <div>
       <h1>{count}</h1>
       <button onClick={() => setCount(count + 1)}>Click</button>
-
       <h2>{title}</h2>
       <button onClick={()=> {
-         // state는 등호로 상태를 변경할 수 없다.
+        // state는 등호로 상태를 변경할 수 없다.
         // state 변경 함수에는 이름 앞에 set을 붙여준다.
         // state 변경 함수로 값을 변경해야 html이 재랜더링 된다
+        // state변경 함수는 state가 가지고 있는 값을 비교해서
+        // 같은 값이면 변경하지 않고, 
+        // 다른 값이면 state함수 호출 값으로 변경 시켜준다.
         let titleCopy = [...title];
         titleCopy = '리액트 너무 재밌어요!';
+        // console.log(title == titleCopy)
         setTitle(titleCopy)
       }}>제목 변경</button>
+
+      <h3>{name}</h3>
+      <button onClick={() => {
+        let nameCopy = [...name];
+        nameCopy[0] = '이예진';
+        setName(nameCopy)  
+      }}>이름 변경</button>
     </div>
   );
 };
-
-export default State;
