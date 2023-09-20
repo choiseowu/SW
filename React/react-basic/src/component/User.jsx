772480151fb7b5a17aa834aabd013a2e7ab2fb45
choiseowu <chlthdud28@naver.com>
2023-09-20 13:12:02 +0900

    이름 변경

--- React/react-basic/src/component/User.jsx
+++ React/react-basic/src/component/User.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 function User(){
-  let name = 'Yejin';
+  let name = 'Seowoo';
   return(
     <div>
-      <h1>안녕하세요, {name == 'Yejin' ? name : 'user 없음'}님!</h1>
+      <h1>안녕하세요, {name == 'Seowoo' ? name : 'user 없음'}님!</h1>
     </div>
   )
 }
