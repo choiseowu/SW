34a47eb406e72088f5f036dceeebef3e0103dd1f
choiseowu <chlthdud28@naver.com>
2023-09-20 13:22:40 +0900

    ✨ setBg 상태변경 함수로 배경색 변경

--- React/react-basic/src/component/State.jsx
+++ React/react-basic/src/component/State.jsx
@@ -5,11 +5,14 @@ import { useState } from 'react';
 // React에서 상태를 관리하기 위한 Hook 중에 하나
 // state를 사용해서 상태를 변경하면, React가 
 // 변경된 상태를 감지해서 자동으로 컴포넌트를 재랜더링 한다.
+
 // useState()
 // Javascript 변수 다루듯이 값을 직정 수정 할 수 없다.
 // useState() 함수를 이용해서 값을 변경해야 한다.
+
 // 1. import {useState} from 'react'
 // 2. let [변수이름, set변수이름] = useState(초기값)
+
 const State = () => {
   // let num = 0;
   let [count, setCount] = useState(0);
@@ -21,15 +24,17 @@ const State = () => {
   let changeBg = () => {
     // 삼항연산자로 현재 배경색이 white라면
     // 다른색으로 바꿔주고, 아니라면 white로 바꿔라
-
+    let newBg = bg == 'white' ? 'green' : 'white';
+    setBg(newBg);
   }
-  
+
   // count : 해당 상태값을 의미하는 변수
   // setCount : 해당 상태값을 변경하는 함수
   // 이 함수를 호출할 때 변경된 값을 인수로 전달한다.
   // (0) : 초기 상태 값 / 컴포넌트가 처음 랜더링 될 때 사용되는 값
   return (
-    <div>
+    <div style={{backgroundColor : bg}}>
+      <button onClick={changeBg}>배경색 변경</button>
       <h1>{count}</h1>
       <button onClick={() => setCount(count + 1)}>Click</button>
       <h2>{title}</h2>
@@ -49,9 +54,12 @@ const State = () => {
       <h3>{name}</h3>
       <button onClick={() => {
         let nameCopy = [...name];
-        nameCopy[0] = '이예진';
+        nameCopy[0] = '최서우';
         setName(nameCopy)  
       }}>이름 변경</button>
     </div>
   );
 };
+
+
+export default State;
