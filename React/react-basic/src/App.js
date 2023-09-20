7b415e0afa9a5834387465a50855fccd30843971
choiseowu <chlthdud28@naver.com>
2023-09-20 12:55:06 +0900

    ✨ Box component 생성

--- React/react-basic/src/App.js
+++ React/react-basic/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Box from './component/Box';
 
 function App() {
   return (
     <div className="App">
-      
+
+      <Box></Box>
+      <Box/>
     </div>
   );
 }
-
-export default App;
