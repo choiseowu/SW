ffba3baae1c837930f405a0627d20940d73a495b
choiseowu <chlthdud28@naver.com>
2023-09-21 15:17:17 +0900

    ✨ header 부분 Nav component 완성

--- React/react-basic/src/App.js
+++ React/react-basic/src/App.js
@@ -1,21 +1,11 @@
 import './App.css';
-// import User from './component/User';
-// import State from './component/State';
-import Map from './component/Map';
+import Nav from './component/Header/Nav';
 
 function App() {
-  let element = <h1>안녕!</h1>;
-  // HTML 코드 return() 안에 작성 해야한다.
-  // return() 안에는 무조건 하나의 태그로 시작해서
-  // 하나의 태그로 끝나야 한다.(태그를 병렬로 기입 X)
   return (
-    // class -> className 
-    // 리액트가 Javascript class 문법으로 인식하기 때문
-    <div className="App">
-      {/* <User/>
-      <State/> */}
-      <Map/>
+    <div className="color">
+      <Nav/>
     </div>
   )};
 
-  export default Map;
+  export default App;
