5c7aa750367e11487a78a290a9e0ddb0d1319850
choiseowu <chlthdud28@naver.com>
2023-09-25 14:15:49 +0900

    ✨ 가위바위보 마크업 완료

--- React/rock/src/component/Box.jsx
+++ React/rock/src/component/Box.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Box.css';
 
-const Box = () => {
+const Box = (props) => {
   return (
     <div>
-    
-    </div>
+      <h1>{props.title}</h1>
+      <h2>Rock</h2>
+      <img src="https://cdn.topstarnews.net/news/photo/202004/769312_495856_1926.jpg" alt="바위 이미지" />
+      <h2>win</h2>
+    </div> 
   );
 };
 
